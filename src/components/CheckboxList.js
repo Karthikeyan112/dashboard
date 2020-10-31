00a6e3e5020ef77d9f23d4dc99b7f287644218bb
karthikeyan.k1 <karthikeyan.k1@partner.samsung.com>
2020-10-31 22:45:58 +0530

    add expand and collapse funtionality

--- src/components/CheckboxList.js
+++ src/components/CheckboxList.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import { Checkbox, Row, Col } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Checkbox } from 'antd';
 
 const CheckboxList = ({ defaultValue, title, plainOptions }) => {
+  const [showAll, setShowAll] = useState(false);
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    showAll ? setOptions(plainOptions) : setOptions(plainOptions.slice(0, 2))
+  }, [showAll])
   console.log(defaultValue);
   return (
     <div className='sidebar__container'>
       <p>{title}</p>
-      <Checkbox.Group style={{ width: '100%' }} options={plainOptions} value={defaultValue} />
+      <Checkbox.Group style={{ width: '100%' }} options={options} value={defaultValue} />
+      <p className='sidebar__expand' onClick={() => setShowAll(!showAll)}>{showAll ? 'Collapse' : 'Expand All'}</p>
     </div>
   )
 }
