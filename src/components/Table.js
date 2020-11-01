6d02c25b1aa1855177fc43a5e28111cd02c95714
karthikeyan.k1 <karthikeyan.k1@partner.samsung.com>
2020-11-01 08:37:17 +0530

    change data and add filter to table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,51 +1,74 @@
 import React, { useEffect, useState } from 'react';
-import { Table, Space } from 'antd';
+import { Table, Space, Switch } from 'antd';
 
 const TableComponent = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     fetch(`${process.env.PUBLIC_URL}/data.json`).then(res => res.json())
       .then(res => {
-        console.log(res);
         setData(res.data);
       })
       .catch(err => console.log(err))
   }, []);
 
   const filterOptions = (arr, prop) => {
-    return arr.reduce((acc, item) => [...acc, { text: item[prop], value: item[prop] }], [])
+    return Array.from(new Set(arr.reduce((acc, item) => [...acc, item[prop]], []))).map(item => ({ text: item, value: item }));
   }
 
   const columns = [
+    {
+      title: 'Type',
+      dataIndex: 'type',
+      key: 'type',
+      filters: filterOptions(data, 'type'),
+      // filteredValue: filteredInfo.name || null,
+      onFilter: (value, record) => record.type.includes(value),
+      sorter: (a, b) => a.type.length - b.type.length,
+      // sortOrder: sortedInfo.columnKey === 'name' && sortedInfo.order,
+      ellipsis: true,
+    },
+    {
+      title: 'Risk Level',
+      dataIndex: 'riskLevel',
+      key: 'riskLevel',
+      filters: filterOptions(data, 'riskLevel'),
+      onFilter: (value, record) => {
+        console.log(record);
+        return record.riskLevel.includes(value)
+      },
+      ellipsis: true,
+    },
     {
       title: 'Name',
       dataIndex: 'name',
       key: 'name',
       filters: filterOptions(data, 'name'),
-      // filteredValue: filteredInfo.name || null,
       onFilter: (value, record) => record.name.includes(value),
       sorter: (a, b) => a.name.length - b.name.length,
-      // sortOrder: sortedInfo.columnKey === 'name' && sortedInfo.order,
       ellipsis: true,
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-      sorter: (a, b) => a.age - b.age,
-      // sortOrder: sortedInfo.columnKey === 'age' && sortedInfo.order,
+      title: 'Matches',
+      dataIndex: 'matches',
+      key: 'matches',
+      sorter: (a, b) => a.matches - b.matches,
       ellipsis: true,
     },
     {
       title: 'Address',
       dataIndex: 'address',
       key: 'address',
-      filters: filterOptions(data, 'address'),
-      // filteredValue: filteredInfo.address || null,
+      filters: filterOptions(data, 'country'),
       onFilter: (value, record) => record.address.includes(value),
       sorter: (a, b) => a.address.length - b.address.length,
-      // sortOrder: sortedInfo.columnKey === 'address' && sortedInfo.order,
       ellipsis: true,
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+      ellipsis: true,
+      render: (status) => <Switch checked={status} />
     },
   ];
   return (
